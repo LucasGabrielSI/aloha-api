1de37caec8df1ad18a36f3b9e570bf660e7df189
Lucas Gabriel <lucas.gabriel@deway.com.br>
2019-09-14 16:01:02 -0300

    fix bug in return of services

--- backend_mobile/user/viewsets.py
+++ backend_mobile/user/viewsets.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate
+from django.http import JsonResponse
 
 from users.models import User
 from rest_framework import status
@@ -24,13 +25,12 @@ class UserViewSet(ModelViewSet):
         user_is_exist = User.objects.filter(email=email)
 
         if len(user_is_exist) > 0:
-            return Response({'Message': 'Usuário já cadastrado', 'success': False},
-                            status=status.HTTP_404_NOT_FOUND)
+            return JsonResponse({'Message': 'Usuário já cadastrado', 'success': False, 'status': 400})
 
         new_user = User.objects.create_user(username=email, email=email, name=name)
         new_user.set_password(password)
         new_user.save()
-        return Response({'message': 'Cadastro efetuado com sucesso', 'success': True}, status=status.HTTP_201_CREATED)
+        return JsonResponse({'message': 'Cadastro efetuado com sucesso', 'success': True, 'status': 201})
 
     @action(methods=['POST'], detail=False, permission_classes=[AllowAny])
     def sign_in(self, request):
@@ -38,17 +38,16 @@ class UserViewSet(ModelViewSet):
         password = request.data['password']
 
         if not email or not password:
-            return Response({'message': 'Informe os dados para que consiga efetuar o login', 'success': False},
-                            status=status.HTTP_400_BAD_REQUEST)
-
+            return JsonResponse({'message': 'Informe os dados para que consiga efetuar o login', 'success': False,
+                                 'status': 400})
         try:
             user_is_exist = User.objects.get(email=email)
             if user_is_exist:
                 user = authenticate(username=email, password=password)
-                token = Token.objects.create(user=user)
-                return Response({'message': 'Login efetuado com sucesso', 'success': True, 'token': str(token)},
-                                status=status.HTTP_200_OK)
+                token = Token.objects.get_or_create(user=user)
+                return JsonResponse({'message': 'Login efetuado com sucesso', 'success': True, 'token': str(token),
+                                     'status': 200})
         except Exception as exc:
             print(exc)
-            return Response({'message': 'Nenhum usuário encontrado ou os dados foram informados errados',
-                             'success': False}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return JsonResponse({'message': 'Nenhum usuário encontrado ou os dados foram informados errados',
+                                 'status': 400, 'success': False})
