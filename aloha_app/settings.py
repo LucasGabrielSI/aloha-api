6a2029d90220ef20f2d70079f9d87e0a285df09a
Lucas Gabriel <lucas.gabriel@deway.com.br>
2019-09-16 17:00:46 -0300

    heroku login

--- aloha_app/settings.py
+++ aloha_app/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,18 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
-#
-# MEDIA_URL = '/static/media/'
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#
-# STATIC_URL = '/static/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 
-PROJECT_ROOT = os.path.dirname (os.path.abspath('__ file__'))
+MEDIA_URL = '/static/media/'
 
-STATIC_URL = '/ static /'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join (PROJECT_ROOT, 'static')
-
-django_heroku.settings(locals())
+STATIC_URL = '/static/static/'
