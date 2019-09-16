0a638df77e0101fcb790006a9eff73b230c73c5f
Lucas Gabriel <lucas.gabriel@deway.com.br>
2019-09-16 10:37:55 -0300

    deploy application

--- aloha_app/urls.py
+++ aloha_app/urls.py
@@ -13,5 +13,6 @@ admin.site.site_title = 'Aloha'
 urlpatterns = [
     path('', admin.site.urls),
     path('api/', include(router.urls)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+# static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
